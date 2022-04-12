b35e07c5516a645cf4528464ca7e4d0821a2b03b
gogiranyan <91252747+gogiranyan@users.noreply.github.com>
2022-04-12 22:21:14 +0800

    start!!

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+  //1.載入express模組
+let app = express();
+  // 2.使用express
+app.get('/', function (req, res) {
+    res.send('Hello')
+})
+  // 5.首頁
+let port = 3000;
+  //3.設定port位置
+// app.listen(port);
+  // 4.監聽 port
+
+  app.listen(3000, '0.0.0.0', function() {
+    console.log('Listening to port:  ' + 3000);
+});
